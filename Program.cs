55c6d7fcd5e05d16c255d39bf29c28004ecb508d
NwizuEmmanuel <nwizuemmanuel200@gmail.com>
2023-10-31 01:54:05 +0800

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿namespace Poet
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string words = "Mark is aboy in antartica of Silic. Mark was a child in brewing anger with ada.";
+            string[] stringSeparator = new string[] { " " };
+
+            string[] result = words.Split(stringSeparator, StringSplitOptions.None);
+            string[] finalResult = Array.FindAll(result, item => item.StartsWith("a"));
+
+            int lineCounter = 0;
+            foreach (string str in finalResult)
+            {
+                Console.Write(str + " ");
+                lineCounter++;
+                if (lineCounter == 3)
+                {
+                    Console.WriteLine();
+                    lineCounter = 0;
+                }
+
+            }
+        }
+    }
+}
